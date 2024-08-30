bb13adc699d69848203c13b46d263b7e09a7a20f
Cristian Andrione <cristian.andrione@gmail.com>
2024-08-30 18:21:36 -0300

    add python script

--- scripts/main.py
+++ scripts/main.py
@@ -4,8 +4,5 @@ import os
 secret_key = os.getenv('MY_SECRET_KEY')
 variable = os.getenv('ALIEN')
 
-if secret_key and variable:
-    print(f"Secret key loaded successfully!\n{secret_key}\nSecret key loaded successfully!{variable}")
-else:
-    print("Failed to load secret key.")
+print(f"Secret key loaded successfully!\n{secret_key}\nSecret key loaded successfully!{variable}")
 
