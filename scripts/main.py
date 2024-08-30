5ccaaebeac401eb2a67d2b7b4f69c9efef01845f
Cristian Andrione <cristian.andrione@gmail.com>
2024-08-30 18:14:42 -0300

    add python script

--- scripts/main.py
+++ scripts/main.py
@@ -2,9 +2,10 @@ import os
 
 # Leer el secret desde la variable de entorno
 secret_key = os.getenv('MY_SECRET_KEY')
+variable = os.getenv('ALIEN')
 
-if secret_key:
-    print(f"Secret key loaded successfully!\n{secret_key}")
+if secret_key and variable:
+    print(f"Secret key loaded successfully!\n{secret_key}\nSecret key loaded successfully!{variable}")
 else:
     print("Failed to load secret key.")
 
