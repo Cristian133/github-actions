04c0fe21ce824149ba938b18450a86af7f966f6e
Cristian Andrione <cristian.andrione@gmail.com>
2024-08-31 17:55:12 -0300

    add reading commit msn

--- scripts/main.py
+++ scripts/main.py
@@ -4,6 +4,7 @@ import os
 secret_key = os.getenv('MY_SECRET_KEY')
 variable = os.getenv('ALIEN')
 
+print("Hola mundo python")
 print(f"Secret key loaded successfully!{secret_key}")
 print(f"Variable key loaded successfully!{variable}")
 
