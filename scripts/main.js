038b492b4b70594d409b19f73366fdb9004c7760
Cristian Andrione <cristian.andrione@gmail.com>
2024-08-31 17:31:30 -0300

    add js script

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,6 @@
+const secretKey = process.env.MY_SECRET_KEY;
+const variable = process.env.ALIEN;
+
 console.log("Hola mundo javascript")
+console.log(`The secret key is: ${secretKey}`);
+console.log(`The secret key is: ${variable}`);
